8f3380d9b208ad2f05a393242f6120938a3f5baf
Kyle Stewart <kstewart83@gmail.com>
2025-12-21 08:19:26 -0500

    feat(analytics): add WebVitals component to layout
    Enables automatic collection of Core Web Vitals (LCP, FCP, CLS, INP, TTFB)
    and logging to otel_metrics table.

--- rubigo-react/src/app/layout.tsx
+++ rubigo-react/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Outfit } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import { WebVitals } from "@/components/web-vitals";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,6 +35,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${outfit.variable} antialiased`}
       >
+        <WebVitals />
         <ThemeProvider>
           {children}
         </ThemeProvider>
